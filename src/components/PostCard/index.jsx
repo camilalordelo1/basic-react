68b5baad474adfe3fafc5c19894ce93a149b75df
Camila Lordelo <camilalordelo15@gmail.com>
2021-09-23 13:28:58 -0300

    first commit

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export const PostCard = ({title, body, id, cover}) => (
+        <div className="post">
+        <img src={cover} alt={title} />
+            <div className="post-content">
+                <h1> {title}</h1> 
+                <p>{body}</p>
+            </div>
+        </div>
+);
+
+
+
